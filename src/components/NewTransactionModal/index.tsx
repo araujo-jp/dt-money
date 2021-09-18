5303aaffb6cee371f5fec45adca8689ab24ac633
Joao Paulo Araujo <paulointer077@gmail.com>
2021-09-18 18:15:32 -0400

    feat: add data to the api and listing transactions and seeds

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -5,6 +5,7 @@ import { Container, TransactionTypeContainer, RadioBox } from './styles'
 import closeImg from '../../assets/close.svg'
 import incomeImg from '../../assets/income.svg'
 import outcomeImg from '../../assets/outcome.svg'
+import { api } from '../../services/api'
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -20,6 +21,15 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
   function handleCreateNewTransaction(event: FormEvent) {
     event.preventDefault()
   }
+
+  const data = {
+    title,
+    category,
+    value,
+    type
+  }
+
+  // api.post('/transactions', data)
 
   return (
     <Modal
